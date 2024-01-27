aaea7785c2a133fcf6169adf2fe321f133f1d080
Vlad Chikov <chikovvlad@yandex.com>
2024-01-27 03:34:16 +0300

    feat: сверстать header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import clsx from 'clsx';
+import { paths } from '../../utils/paths';
+import s from './Header.module.css';
+
+export function Header() {
+  // Временное решение для переключения темы
+  const [theme, setTheme] = useState('light');
+
+  return (
+    <header className={s.header}>
+      <div className={s.container}>
+        <Link to={paths.homePage} className={s.logoLink}>
+          <div className={s.logoImg} />
+          <h1 className={s.logoText}>Фото by Unsplash</h1>
+        </Link>
+        <div className={s.navContainer}>
+          <NavLink
+            to={paths.loginPage}
+            className={({ isActive }) =>
+              !isActive ? s.navLink : s.navLinkActive
+            }
+          >
+            Login
+          </NavLink>
+          <NavLink
+            to={paths.registrationPage}
+            className={({ isActive }) =>
+              !isActive ? s.navLink : s.navLinkActive
+            }
+          >
+            Registration
+          </NavLink>
+        </div>
+        <div className={s.themeButtonsContainer}>
+          <button
+            type="button"
+            className={clsx(s.lightThemeButton, {
+              [s.lightThemeButtonActive]: theme === 'light'
+            })}
+            onClick={() => setTheme('light')}
+          />
+          <button
+            type="button"
+            className={clsx(s.darkThemeButton, {
+              [s.darkThemeButtonActive]: theme === 'dark'
+            })}
+            onClick={() => setTheme('dark')}
+          />
+        </div>
+      </div>
+    </header>
+  );
+}
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,11 +1,5 @@
-import { Outlet } from 'react-router-dom';
 import './HomePage.module.css';
 
 export function HomePage() {
-  return (
-    <>
-      <p>HomePage</p>
-      <Outlet />
-    </>
-  );
+  return <p>HomePage</p>;
 }
