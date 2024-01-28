6f4a3bac87ca6106b123b57fadfac8d25253ca6e
Vlad Chikov <chikovvlad@yandex.com>
2024-01-28 19:10:30 +0300

    feat: сверстать форму с поиском фото

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,5 +1,42 @@
-import './HomePage.module.css';
+import { useState } from 'react';
+import clsx from 'clsx';
+import s from './HomePage.module.css';
 
 export function HomePage() {
-  return <p>HomePage</p>;
+  //Временное решение для показа текста ошибки
+  const [error, setError] = useState(false);
+  function handleSubmit(evt: React.FormEvent<HTMLFormElement>): void {
+    evt.preventDefault();
+    setError(!error);
+  }
+
+  return (
+    <>
+      <section className={s.searchSection}>
+        <form className={s.searchForm} onSubmit={handleSubmit}>
+          <label className={s.inputContainer}>
+            <input
+              className={s.input}
+              type="text"
+              placeholder="Введите ключевое слово(а) для поиска фото"
+            />
+            <span
+              className={clsx(s.inputErrorText, {
+                [s.inputErrorTextVisible]: !error
+              })}
+            >
+              Минимальное количество символов: 2
+            </span>
+          </label>
+
+          <button type="submit" className={s.button}>
+            Искать
+          </button>
+        </form>
+      </section>
+      <section className={s.photoSection}>
+        <form />
+      </section>
+    </>
+  );
 }
