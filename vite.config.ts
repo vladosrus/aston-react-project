f596016d383bc3acba95febf93ed0c48a8ae2dfc
Vlad Chikov <chikovvlad@yandex.com>
2024-02-01 20:50:30 +0300

    feat: сверстать страницу с историей поиска;

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 //Здесь важен именно экспорт по умолчанию для работы приложения
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   base: '/aston-react-project',
-  plugins: [react()]
+  plugins: [react(), svgr()]
 });
