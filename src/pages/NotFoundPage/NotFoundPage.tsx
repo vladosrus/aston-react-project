b8790624ae4db94f6393fedc12b527a8542bc611
Vlad Chikov <chikovvlad@yandex.com>
2024-02-01 22:47:05 +0300

    feat: сверстать страницу 404

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,16 @@
 import { memo } from 'react';
+import NotFoundImage from '../../shared/ui/assets/notFoundPage_img.svg?react';
+import { Logo } from '../../widgets/Header/ui/Logo/Logo';
+
+import s from './NotFoundPage.module.css';
 
 const NotFoundPage = memo(() => {
-  return <p>NotFoundPage</p>;
+  return (
+    <section className={s.section}>
+      <Logo />
+      <NotFoundImage className={s.img} />
+    </section>
+  );
 });
 
 // Без дефолтного эспорта будут громоздкие импорты для lazy
