44c4e97b8a51f3f6ad4065a95b2239d73102b9f7
Vlad Chikov <chikovvlad@yandex.com>
2024-02-02 14:02:51 +0300

    feat: сверстать страницу с информацией о фото

---
+++ src/widgets/PhotoDetails/PhotoDetails.tsx
@@ -0,0 +1,60 @@
+import { ReactNode, memo } from 'react';
+import { PhotoInfo } from '../../pages/DetailsPage/DetailsPage';
+import { LikeButton } from '../../shared/ui/LikeButton/LikeButton';
+
+import s from './PhotoDetails.module.css';
+
+export const PhotoDetails = memo(({ photoInfo }: { photoInfo: PhotoInfo }) => {
+  const photoDescription: {
+    category: string;
+    description: string | ReactNode;
+  }[] = [
+    { category: 'Создатель:', description: photoInfo.user.name },
+    { category: 'Страна:', description: photoInfo.user.location },
+    {
+      category: 'Описание:',
+      description:
+        photoInfo.description?.length > photoInfo.alt_description?.length
+          ? photoInfo.description
+          : photoInfo.alt_description
+    },
+    {
+      category: 'Профиль создателя в Unsplash:',
+      description: (
+        <a
+          href={photoInfo.user.links.html}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={s.link}
+        >{`@${photoInfo.user.username}`}</a>
+      )
+    }
+  ];
+
+  return (
+    <article className={s.article}>
+      <img
+        className={s.img}
+        src={photoInfo.urls.raw}
+        alt={
+          photoInfo.description
+            ? photoInfo.description
+            : photoInfo.alt_description
+        }
+      />
+      <ul className={s.descriptionList}>
+        {photoDescription.map((item) => {
+          return (
+            <li key={item.category} className={s.descriptionListItem}>
+              <span className={s.category}>{item.category}</span>
+              <p className={s.description}>{item.description}</p>
+            </li>
+          );
+        })}
+        <li className={s.descriptionListItem}>
+          <LikeButton />
+        </li>
+      </ul>
+    </article>
+  );
+});
