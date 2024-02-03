01470fabe663439a8910d3b797bf067ae5609f7b
Vlad Chikov <chikovvlad@yandex.com>
2024-02-03 12:24:50 +0300

    fix: вернуть удалённые папки обратно с изменёнными названиями (2)

---
+++ src/shared/ui/section/section.tsx
@@ -0,0 +1,18 @@
+import { memo } from 'react';
+import clsx from 'clsx';
+import s from './section.module.css';
+
+type Props = { type: 'main' | 'top'; children?: JSX.Element };
+
+export const Section = memo(({ type, children }: Props) => {
+  return (
+    <section
+      className={clsx({
+        [s.mainSection]: type === 'main',
+        [s.topSection]: type === 'top'
+      })}
+    >
+      {children}
+    </section>
+  );
+});
