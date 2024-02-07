6955d74049e6e282934215a24a39aea4fac46e9a
Vlad <chikovvlad@yandex.com>
2024-02-07 19:15:52 +0000

    Updates

--- assets/login-page-9sNU1ReP.js
+++
@@ -1,1 +0,0 @@
-import{r as s,a as e,j as t}from"./index-iFFN4YMq.js";import{S as o}from"./section-dP7cVYI5.js";import{A as m}from"./auth-form-AfrfS2ry.js";import"./clsx-Zbgk8kpT.js";const x=s.memo(()=>{const{handleLogin:r}=e();return t.jsxs(t.Fragment,{children:[t.jsx(o,{type:"top"}),t.jsx(o,{type:"main",children:t.jsx(m,{name:"Авторизация",onSubmit:r})})]})});export{x as default};
---
+++ assets/use-typed-selector-cLSHM1Pf.js
@@ -0,0 +1,1 @@
+import{o as e}from"./index-r8gq01n7.js";const r=e;export{r as u};
