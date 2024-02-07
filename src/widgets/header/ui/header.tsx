6892b4370ff720264daf8a28770f75db348dcf7e
Vlad Chikov <chikovvlad@yandex.com>
2024-02-07 20:58:47 +0300

    feat: стилизовать header под тёмную тему

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -10,22 +10,22 @@ export const Header = memo(() => {
   const { theme, handleThemeButtonClick } = useTheme();
 
   return (
-    <header className={s.header}>
+    <header className={clsx(s.header, { [s.headerDark]: theme === 'dark' })}>
       <div className={s.container}>
         <Logo />
         <Navigation />
         <div className={s.themeButtonsContainer}>
           <button
             type="button"
-            className={clsx(s.lightThemeButton, {
-              [s.lightThemeButtonActive]: theme === 'light'
+            className={clsx(s.themeButton, s.lightThemeButton, {
+              [s.lightThemeButtonActive]: theme === 'dark'
             })}
             onClick={() => handleThemeButtonClick('dark')}
             title="Изменить оформление страницы на светлое"
           />
           <button
             type="button"
-            className={clsx(s.darkThemeButton, {
+            className={clsx(s.themeButton, s.darkThemeButton, {
               [s.darkThemeButtonActive]: theme === 'dark'
             })}
             onClick={() => handleThemeButtonClick('light')}
--- src/widgets/header/ui/logo/logo.tsx
+++ src/widgets/header/ui/logo/logo.tsx
@@ -1,13 +1,20 @@
+import clsx from 'clsx';
 import { memo } from 'react';
 import { Link } from 'react-router-dom';
 import { paths } from '../../../../shared/model/paths';
+import { useTheme } from '../../../../features/theme/lib/use-theme';
+
 import s from './logo.module.css';
 
 export const Logo = memo(() => {
+  const { theme } = useTheme();
+
   return (
     <Link to={paths.homePage} className={s.link}>
-      <div className={s.img} />
-      <h1 className={s.text}>Фото by Unsplash</h1>
+      <div className={clsx(s.img, { [s.imgDark]: theme === 'dark' })} />
+      <h1 className={clsx(s.text, { [s.textDark]: theme === 'dark' })}>
+        Фото by Unsplash
+      </h1>
     </Link>
   );
 });
